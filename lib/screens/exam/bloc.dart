48647ba8f210395df341980188513b0f087870b9
Christian Jayme <christian.jayme26@gmail.com>
2024-02-03 03:30:50 +0800

    -Added addToCount() method -Added fetchData() method

--- lib/screens/exam/bloc.dart
+++ lib/screens/exam/bloc.dart
@@ -5,6 +5,7 @@ import 'package:mobile_exam/core/app/bloc.dart' as base;
 import 'package:mobile_exam/core/extensions/common.dart';
 import 'package:mobile_exam/core/extensions/map.dart';
 import 'package:mobile_exam/core/services/server.dart';
+import 'package:mobile_exam/screens/exam/model/exam.dart';
 
 import 'views/main.dart' as main_view;
 import 'views/loading.dart' as loading_view;
@@ -20,15 +21,26 @@ class Bloc extends base.Bloc {
 
   @override
   void init() async {
-    final arg = context.arguments;
-    final key = arg?.tryGet("key");
     final service = context.server;
+    fetchData(service);
+  }
 
-    if (key != await service.accessKey) {
-      emit(error_view.ViewState());
-      return;
-    }
+  void addToCount() async {
+    final service = context.server;
+    service.addToCount(1);
 
-    emit(main_view.ViewState());
+    emit(loading_view.ViewState());
+
+    await fetchData(service);
+  }
+
+  Future<void> fetchData(Server service) async {
+    Exam exam = Exam.fromJson(await service.data);
+      if(!exam.isSuccess) {
+        emit(error_view.ViewState(exam: exam));
+        return;
+      }
+      emit(main_view.ViewState(exam: exam));
+
   }
 }
