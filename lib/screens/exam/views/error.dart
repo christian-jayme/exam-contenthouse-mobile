3e5072c725fd72f4d22696ac20f1bb783a540167
Christian Jayme <christian.jayme26@gmail.com>
2024-02-03 03:32:06 +0800

    -Modified Exam Screen UI

--- lib/screens/exam/views/error.dart
+++ lib/screens/exam/views/error.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_exam/core/app/view.dart' as base;
+import 'package:mobile_exam/screens/exam/bloc.dart';
+
+import '../model/exam.dart';
 
 class ViewState extends base.ViewState {
+  ViewState({
+    required this.exam,
+  });
+
+  final Exam exam;
+
   @override
   Widget content(BuildContext context) {
     return Center(
@@ -13,13 +22,24 @@ class ViewState extends base.ViewState {
             color: Colors.red,
             size: 48,
           ),
+          const SizedBox(height: 20),
+          Text(
+            (context.strings!.examErrorLabel) + (exam.statusCode.toString() ?? ""),
+          ),
           Padding(
             padding: const EdgeInsets.all(32.0),
             child: Text(
-              context.strings!.hint2ErrorMessage,
+              exam.errorMessage ?? "",
               style: context.texts.headlineMedium,
             ),
           ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              context.bloc.addToCount();
+            },
+            child: Text(context.strings!.examAddButtonLabel),
+          )
         ],
       ),
     );
--- lib/screens/exam/views/main.dart
+++ lib/screens/exam/views/main.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_exam/core/app/view.dart' as base;
+import 'package:mobile_exam/screens/exam/bloc.dart';
+
+import '../model/exam.dart';
 
 class ViewState extends base.ViewState {
+  ViewState({
+    required this.exam,
+  });
+
+  final Exam exam;
+
   @override
   Widget content(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: [
-          const SizedBox(height: 20),
-          Text(context.strings!.hint2Description),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network(
+              exam.image!,
+              width: 400,
+              height: 200,
+              // Adjust width and height based on your image size
+            ),
+            const SizedBox(height: 20),
+            Text(
+              (context.strings!.examCounterLabel) + (exam.count.toString() ?? ""),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              exam.message ?? "",
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                context.bloc.addToCount();
+              },
+              child: Text(context.strings!.examAddButtonLabel),
+            )
+          ],
+        ),
       ),
     );
   }
